6a89bcb2a137a0150d43a638b95f6dfb22ee8d42
Volodymyr Koreniev <vkorenev@gmail.com>
2026-04-20 16:23:18 -0700

    OpenTelemetry upgrade and other fixes

--- app/src/main/scala/com/example/realworld/telemetry.scala
+++ app/src/main/scala/com/example/realworld/telemetry.scala
@@ -5,18 +5,13 @@ import cats.effect.Sync
 import cats.syntax.functor.*
 import io.opentelemetry.instrumentation.hikaricp.v3_0.HikariTelemetry
 import io.opentelemetry.instrumentation.log4j.appender.v2_17.OpenTelemetryAppender
-import io.opentelemetry.instrumentation.runtimemetrics.java17.RuntimeMetrics
+import io.opentelemetry.instrumentation.runtimetelemetry.RuntimeTelemetry
 import org.typelevel.otel4s.oteljava.OtelJava
 
 def registerRuntimeMetrics[F[_]: Sync](otel4s: OtelJava[F]): Resource[F, Unit] =
   Resource
     .fromAutoCloseable(Sync[F].delay {
-      RuntimeMetrics
-        .builder(otel4s.underlying)
-        .enableAllFeatures()
-        .captureGcCause()
-        .emitExperimentalTelemetry()
-        .build()
+      RuntimeTelemetry.create(otel4s.underlying)
     })
     .void
 
